88f94137db706691db15e8cd9e70485e861cc3d8
troquer <x88andres@hotmail.comx88andres@hotmail.comx88andres@hotmail.com>
2018-01-29 00:15:24 -0500

    Initial commit

---
+++ Z-test/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  Z-test
+//
+//  Created by troquer on 28/01/2018.
+//  Copyright © 2018 acvc. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailLabel: UILabel!
+    var receivedTitle: String = "" //This is set from the previous View Controller 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        detailLabel.text = receivedTitle
+        detailLabel.sizeToFit() //This makes text in the label to align top left
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Z-test/ViewController.swift
+++ Z-test/ViewController.swift
@@ -7,17 +7,59 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var arTitles = [String]()
+    var arBodies = [String]()
+    @IBOutlet weak var tableView: UITableView!
+    let detailSegueIdentifier = "ShowDetail"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        let postsService = PostsService()
+        postsService.getPosts(onSuccess: {
+            (data) in
+            do{
+                let json = try JSON(data: data)
+                let textoRespuesta = String(data: data, encoding: String.Encoding.utf8)
+                print(textoRespuesta)
+                self.arTitles =  json.arrayValue.map({$0["title"].stringValue})
+                self.arBodies =  json.arrayValue.map({$0["body"].stringValue})
+                DispatchQueue.main.async{
+                    self.tableView.reloadData()
+                }
+                
+            } catch {
+                print("No pude procesar convertir json en arreglo")
+            }
+        })
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("voy pa ya")
+        let destination = segue.destination as? DetailViewController
+//        let currentCell = tableView.cellForRow(at: tableView.indexPathForSelectedRow!)!
+        
+        //destination?.detailLabel?.text = arBodies[(tableView.indexPathForSelectedRow?.row)!]
+        destination?.receivedTitle = arBodies[(tableView.indexPathForSelectedRow?.row)!]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath)
+        cell.textLabel?.text = arTitles[indexPath.row]
+        
+        return cell
     }
 
 
